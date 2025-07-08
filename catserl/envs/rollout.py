23e39d790a156ddea6d2f41e83c879eb43c5f960
Raghav Thakar <thakarr@oregonstate.edu>
2025-07-07 17:00:04 -0700

    retrieve and retrn extrinsic only rew (intrinsic augments regular rewards)

--- catserl/envs/rollout.py
+++ catserl/envs/rollout.py
@@ -25,15 +25,21 @@ def rollout(env, policy, learn: bool = True) -> Tuple[np.ndarray, int]:
     s, _ = env.reset()
     done, trunc, ep_len = False, False, 0
     ret_vec = None
+    ext_ret_vec = None
 
     while not (done or trunc):
         a = policy.act(s)
-        s2, r_vec, done, trunc, _ = env.step(a)
+        s2, r_vec, done, trunc, info = env.step(a)
 
         if ret_vec is None:
             ret_vec = np.array(r_vec, dtype=np.float32)
         else:
             ret_vec += r_vec
+        
+        if ext_ret_vec is None:
+            ext_ret_vec = np.array(r_vec, dtype=np.float32)
+        else:
+            ext_ret_vec += info["extrinsic"]
 
         if learn and hasattr(policy, "remember"):
             policy.remember(s, a, r_vec, s2, done or trunc)
@@ -43,4 +49,4 @@ def rollout(env, policy, learn: bool = True) -> Tuple[np.ndarray, int]:
         s = s2
         ep_len += 1
 
-    return ret_vec, ep_len
+    return ret_vec, ep_len, ext_ret_vec

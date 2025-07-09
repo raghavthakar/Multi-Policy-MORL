df271f34f28b050bd9b629523cb65d8d4421b104
Raghav Thakar <thakarr@oregonstate.edu>
2025-07-08 20:11:51 -0700

    fix type hint and ext_ret_vec initilisation

--- catserl/envs/rollout.py
+++ catserl/envs/rollout.py
@@ -13,7 +13,7 @@ from typing import Tuple, Any
 import numpy as np
 
 
-def rollout(env, policy, learn: bool = True) -> Tuple[np.ndarray, int]:
+def rollout(env, policy, learn: bool = True) -> Tuple[np.ndarray, int, np.ndarray]:
     """
     Runs ONE episode and (optionally) lets the policy learn online.
 
@@ -21,6 +21,7 @@ def rollout(env, policy, learn: bool = True) -> Tuple[np.ndarray, int]:
     -------
     ret_vec : np.ndarray   -- episode-summed reward vector
     ep_len  : int          -- steps taken
+    ext_ret_vec : np.ndarray -- episode-summed extrinsic reward vector
     """
     s, _ = env.reset()
     done, trunc, ep_len = False, False, 0
@@ -37,7 +38,7 @@ def rollout(env, policy, learn: bool = True) -> Tuple[np.ndarray, int]:
             ret_vec += r_vec
         
         if ext_ret_vec is None:
-            ext_ret_vec = np.array(r_vec, dtype=np.float32)
+            ext_ret_vec = np.array(info["extrinsic"], dtype=np.float32)
         else:
             ext_ret_vec += info["extrinsic"]
 
